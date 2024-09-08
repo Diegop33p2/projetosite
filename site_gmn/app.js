b0113141ef4d53f56581bf3cd8dd4b2b088f72e8
Diego Pereira <diegopereira.com.p2.br@gmail.com>
2024-09-08 12:05:16 -0300

    gym

---
+++ site_gmn/app.js
@@ -0,0 +1,21 @@
+
+function pesquisar() {
+    //Pega a seção HTML onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisas");
+    //Cria uma string vazia para guardar os resultados
+    let resultados = "";
+    //Para cada dado na lista de dados:
+    for(let dado of dados) {
+    //Cria um novo elemento HTML para cada resultado
+      resultados += `
+        <div class="item-resultado">
+          <h2>${dado.nome}</h2>
+          <p class="descricao-meta">${dado.parte_do_corpo}</p>
+          <p class="descricao-meta">${dado.descricao}</p>
+          <a href="${dado.link}" target="_blank">Link</a>
+        </div>
+      `;
+    }
+    //Coloca todos os resultados na seção HTML
+    section.innerHTML = resultados;
+  }
