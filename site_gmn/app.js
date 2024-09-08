da0db739b64f715976bfb76bab37ea7d9439186d
Diego Pereira <diegopereira.com.p2.br@gmail.com>
2024-09-08 12:20:56 -0300

    ex

--- site_gmn/app.js
+++ site_gmn/app.js
@@ -2,6 +2,8 @@
 function pesquisar() {
     //Pega a seção HTML onde os resultados serão exibidos
     let section = document.getElementById("resultados-pesquisas");
+    //Pega o texto que será digitado
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
     //Cria uma string vazia para guardar os resultados
     let resultados = "";
     //Para cada dado na lista de dados:
